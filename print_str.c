78438d901d19246adc5f83890c2281a6be587091
Jeff Mofor <allojeff444@gmail.com>
2023-04-23 09:02:05 -0700

    Edited print string

--- print_str.c
+++ print_str.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * @brief prints out a string
- * 
- * @param s: string to be printed
+ * print_string - prints out a string
+ *
+ * @s: string to be printed
  */
 void print_string(const char *s)
 {
-    int j = 0;
+	int j = 0;
 
-    while (s[j] != '\0')
-    {
-        _putchar(s[j]);
-        j++;
-    }
+	while (s[j] != '\0')
+	{
+		_putchar(s[j]);
+		j++;
+	}
 }
