f5d63755b954d3e05aaa83b449f4078092abcdb9
LeoYoumbi <Leoyoumbi83@gmail.com>
2023-04-23 15:48:39 +0100

    Fix indent: print_float.c

--- print_float.c
+++ print_float.c
@@ -1,49 +1,47 @@
 #include "main.h"
 /**
- * @brief printf a floating point number to six decimal places
+ * print_float - a floating point number to six decimal places
  * 
- * @param n: float 
+ * @n: float
  */
 void print_float(double n)
 {
-    int i, j;
+	int i, j;
 
-    i = (int)n;
-    print_nums(i);
-    n -= i;
-    _putchar('.');
+	i = (int)n;
+	print_nums(i);
+	n -= i;
+	_putchar('.');
 
-    for (j = 0; j < 6; j++)
-    {
-        n *= 10;
-    }
-    print_nums(n);
-
+	for (j = 0; j < 6; j++)
+	{
+	n *= 10;
+	}
+	print_nums(n);
 }
 
 /**
- * @brief prints a float to a specified number of decimal places
+ * print_float_spec - prints a float to a specified number of decimal places
  * 
- * @param n: float input
- * @param c: specified number of decimal places
+ * @n: float input
+ * @c: specified number of decimal places
  */
 void print_float_spec(double n, char c)
 {
-    int pow, i, j;
+	int pow, i, j;
 
-    char s[1];
-    s[0] = c;
-    pow = _atoi(s);
+	char s[1];
+	s[0] = c;
+	pow = _atoi(s);
 
-    i = (int)n;
-    print_nums(i);
-    n -= i;
-    _putchar('.');
+	i = (int)n;
+	print_nums(i);
+	n -= i;
+	_putchar('.');
 
-    for (j = 0; j < pow; j++)
-    {
-        n *= 10;
-    }
-    print_nums(n);
-    
+	for (j = 0; j < pow; j++)
+	{
+		n *= 10;
+	}
+	print_nums(n);
 }
