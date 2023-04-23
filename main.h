5fa81d01acaa76aadc48dbf63fc271b982452609
Jeff Mofor <allojeff444@gmail.com>
2023-04-23 10:05:24 -0400

    Edited main

--- main.h
+++ main.h
@@ -17,27 +17,4 @@ int _atoi(char *s);
 void print_hex_x_caps(long long n);
 void scientific_notation(double n);
 void scientific_notation_caps(double n);
-
-//forget this
-char _getchar(void);
-
-//new
-
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-    const char *op;
-    int (*f)(int a, int b);
-} op_t;
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(const char *s))(int, int);
 #endif
