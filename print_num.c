6b6ec3e171cf2b4b3eb8794533b06f3a33c131db
LeoYoumbi <Leoyoumbi83@gmail.com>
2023-04-23 15:55:26 +0100

    Fix indent: print_num.c

--- print_num.c
+++ print_num.c
@@ -1,50 +1,50 @@
 #include "main.h"
 
 /**
- * @brief prints an integer
- * 
- * @param n: integer input
+ * print_nums - prints an integer
+ *
+ * @n: integer input
  */
 void print_nums(int n)
 {
-    int pow, fact, tracker, i;
+	int pow, fact, tracker, i;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-    pow = 10;
-    while ((pow * 10) <= n)
-        pow *= 10;
-    
-    while (n >= 10)
-    {
-        fact = n / pow;
-        n -= (pow * fact);
-        _putchar(fact + '0');
-        if (n == 0)
-        {
-            tracker = 0;
-            while (pow != 1)
-            {
-                pow /= 10;
-                tracker++;
-            }
-            break;
-        }
-        pow /= 10;
-    }
-    if (n != 0)
-    {
-        _putchar(n + '0');
-    }
-    else
-    {
-        for (i = 0; i < tracker; i++)
-        {
-            _putchar('0');
-        }
-    }
+	pow = 10;
+	while ((pow * 10) <= n)
+		pow *= 10;
+
+	while (n >= 10)
+	{
+		fact = n / pow;
+		n -= (pow * fact);
+		_putchar(fact + '0');
+		if (n == 0)
+		{
+			tracker = 0;
+			while (pow != 1)
+			{
+				pow /= 10;
+				tracker++;
+			}
+			break;
+		}
+		pow /= 10;
+	}
+	if (n != 0)
+	{
+		_putchar(n + '0');
+	}
+	else
+	{
+		for (i = 0; i < tracker; i++)
+		{
+			_putchar('0');
+		}
+	}
 }
