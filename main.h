45986a7a7380b82547a375b8b250e4194136b4d1
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-04-24 01:32:44 -0700

    Fix _printf in main

--- main.h
+++ main.h
@@ -30,9 +30,10 @@ typedef struct fmt
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
 } fmt_t;
-
+int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
+
 int _putchar(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 void print_buffer(char buffer[], int *bufferIndex);
@@ -46,7 +47,6 @@ int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int _printf(char const *s, ...);
 int _atoi(char *s);
 void print_hex_x_caps(long n);
 
