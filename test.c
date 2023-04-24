876f364b95d65835059e1e4e0392d140f10c93d4
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-04-24 03:18:43 -0700

    ...

--- test.c
+++ test.c
@@ -7,7 +7,10 @@
 int main(void)
 {
 	char *name = "Jeff";
+	char a = 'c';
 
-	_printf("My name is %s", name);
+	_printf("My name is %s.  ", name);
+	_printf("%c  ", a);
+	_printf("%%", a);
 	return (0);
 }
