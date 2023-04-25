91a825ec5d893abfb20bd87cd4bdf20cc7d6c2d2
Youmbi Leo <Leoyoumbi83@gmail.com>
2023-04-25 16:06:19 +0100

    Forgot ';'

--- main.h
+++ main.h
@@ -49,7 +49,7 @@ int print_int(va_list types, char buffer[],
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size);
 int _atoi(char *s);
 void print_hex_x_caps(long n);
 
